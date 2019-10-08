a874d264acfa1cd1474a583b30ffd9e13ee60ce8
Danilo Polastri <danilopolastri.ti@gmail.com>
2019-10-08 16:52:29 -0300

    Struts 2 = Processando Formulário

---
+++ struts-projeto/src/com/br/ebix/struts/action/Register.java
@@ -0,0 +1,26 @@
+package com.br.ebix.struts.action;
+
+import com.br.ebix.struts.model.Person;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Register extends ActionSupport{
+	
+	 private static final long serialVersionUID = 1L;
+	    
+	    private Person personBean;
+
+
+	    public String execute() throws Exception {
+	        //call Service class to store personBean's state in database
+	        
+	        return SUCCESS;
+	    }
+	    
+	    public Person getPersonBean() {
+	        return personBean;
+	    }
+	    
+	    public void setPersonBean(Person person) {
+	        personBean = person;
+	    }
+}
